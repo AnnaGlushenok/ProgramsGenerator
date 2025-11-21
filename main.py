884a2f7f1784ae37c8c0b173902a4fa924bece08
Anna <annasan14052003@gmail.com>
2025-11-21 13:33:59 +0300

    added sort, get_central_chunks, modified clusterize and clean

--- main.py
+++ main.py
@@ -1,55 +1,45 @@
 import re
-import pdfplumber
 import hdbscan
+import pdfplumber
 import umap
-import requests
+import Config
 import json
-import os
 import numpy as np
-from dotenv import load_dotenv
-from sentence_transformers import SentenceTransformer
-from collections import defaultdict
+
 from openai import OpenAI
+from Config import Config
+from collections import defaultdict
+from sentence_transformers import SentenceTransformer
+from scipy.spatial.distance import cdist
 
-PATTERN_SENTENCE = r'[.!?]\s+'
-TRANSFORMER_MODEL = "intfloat/multilingual-e5-base"
-# base_url="https://api.poe.com/v1"
-
-load_dotenv()
-OPEN_ROUTER_API_KEY = os.getenv("OPEN_ROUTER_API_KEY")
-print(OPEN_ROUTER_API_KEY)
-
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=OPEN_ROUTER_API_KEY,
-)
-
-models = {
-    "GPT": "openai/gpt-4o",  #
-    "Deepseek": "deepseek/deepseek-chat-v3.1:free",  #
-    "Gemini": "google/gemini-2.0-flash-exp:free",  #
-    "Polaris": "openrouter/polaris-alpha"
-}
+client = OpenAI(base_url=Config.URL, api_key=Config.OPEN_ROUTER_API_KEY)
 
 
-def process(path, max_sentences=5):
-    # print("read file")
-    # text = read(path)
-    # v = "Довольно скоро на смену иерархической и сетевой моделям пришла принципиально новая модель данных – реляционная. Реляционная модель данных – логическая модель, основанная на множестве взаимосвязанных именованных отношений. В свою очередь, реляционная база данных – база данных, основанная на реляционной модели данных. Термин «реляционный» означает, что теория основана на математическом понятии «отношение». Отношение – это информационная модель реального объекта предметной области, формально представленная множеством однотипных кортежей. Кортеж отношения представляет собой экземпляр моделируемого объекта, свойства которого определяются значениями соответствующих атрибутов («полей») кортежа. Связи между кортежами отношений (при их наличии) реализуются через простой механизм «внешних ключей», являющихся, по существу, ссылками на атрибуты связываемых кортежей отношений. Переменная отношения находится в первой нормальной форме (1НФ) тогда и только тогда, когда в любом допустимом значении этой переменной каждый кортеж отношения содержит только одно значение для каждого из атрибутов. Согласно определению отношений, любое отношение автоматически уже находится в 1НФ согласно свойствам отношений: в отношении нет одинаковых кортежей; кортежи не упорядочены; атрибуты не упорядочены и различаются по наименованию; все значения атрибутов атомарны. Данные свойства и являются свойствами первой нормальной формы. Если упростить определение, таблица приведена к первой нормальной форме, если в ней отсутствуют повторяющиеся группы, и все значения, хранимые в ней, неделимы (атомарны).Вернемся к примеру. В ходе логического моделирования на первом шаге предложено хранить данные в одном сводном отношении, имеющем следующие атрибуты: Инструкция SELECT, предназначенная для выборки данных из одной или нескольких таблиц или представлений, является основным элементом языка SQL. В простейшем случае результат запроса может полностью повторять содержимое одной таблицы, может представлять собой объединение нескольких таблиц, определенную выборку из исходных данных и т.д. Помимо этого, запросы SELECT активно используются при создании ряда ключевых объектов базы данных (представлений, хранимых процедур и триггеров). Базовая синтаксическая конструкция запроса выглядит следующим образом: SELECT [DISTINCT|ALL] { имя_столбца [AS псевдоним] [,…] |функция_агрегирования [AS псевдоним] [,…] |выражение_для_вычисления_значения [AS псевдоним] [,…] |спецификатор.* } FROM {имя_таблицы [AS псевдоним] [,…] |имя_представления [AS псевдоним][,…]} [WHERE условия_отбора] [GROUP BY имя_столбца [,…]] [HAVING условие] [ORDER BY имя_столбца [ASC | DESC] [,…]]"
-    print("create chunks")
-    # chunks = get_chunks(text, max_sentences)
-    # np.save("chunks.npy", chunks)
+# TODO embeding api
+def process_cash(path, max_sentences=5):
+    print("read chunks")
     chunks = np.load("chunks.npy")
-    print("create embeddings")
-    # embeddings = get_embeddings(chunks)
+    print("read embeddings")
     embeddings = np.load("embeddings.npy")
-    # np.save("embeddings.npy", embeddings)
-    labels = cluster(embeddings)
-    answer(chunks, labels)
+    labels = clusterize(embeddings)
+    answer(embeddings, chunks, labels)
     print("Done!")
 
 
+def process(paths, max_sentences=5):
+    print("Пошло-поехало")
+    texts = ",".join([read(path) for path in paths])
+    chunks = get_chunks(texts, max_sentences)
+    np.save("chunks.npy", chunks)
+    embeddings = get_embeddings(chunks)
+    np.save("embeddings.npy", embeddings)
+    labels = clusterize(embeddings)
+    answer(embeddings, chunks, labels)
+    print("Готово!")
+
+
 def read(path):
+    print(f"read file {path}")
     text = ""
     with pdfplumber.open(path) as pdf:
         for page in pdf.pages:
@@ -60,7 +50,8 @@ def read(path):
 
 
 def get_chunks(text, max_sentences=5):
-    sentences = re.split(PATTERN_SENTENCE, text)
+    print("create chunks")
+    sentences = re.split(Config.PATTERN_SENTENCE, text)
     chunks = []
     for i in range(0, len(sentences), max_sentences):
         chunks.append(" ".join(sentences[i:i + max_sentences]))
@@ -68,13 +59,25 @@ def get_chunks(text, max_sentences=5):
 
 
 def get_embeddings(chunks):
-    model = SentenceTransformer(TRANSFORMER_MODEL)
+    print("create embeddings")
+    model = SentenceTransformer(Config.TRANSFORMER_MODEL)
     embeddings = model.encode(chunks)
     return embeddings
 
 
-def cluster(embeddings):
-    umap_model = umap.UMAP(n_neighbors=15, n_components=15, metric='cosine')
+# TODO найти модель, протестить!
+def get_embeddings_api(chunks):
+    embedding = client.embeddings.create(
+        model="sentence-transformers/paraphrase-minilm-l6-v2",
+        input=chunks,
+        encoding_format="float"
+    )
+    return embedding.data[0].embedding
+
+
+def clusterize(embeddings):
+    print("Кластеризуется")
+    umap_model = umap.UMAP(n_neighbors=20, n_components=15, metric='cosine')
     emb_small = umap_model.fit_transform(embeddings)
 
     clusterer = hdbscan.HDBSCAN(min_cluster_size=4, metric='euclidean')
@@ -84,45 +87,96 @@ def cluster(embeddings):
     return labels
 
 
-def answer(chunks, labels):
-    cluster_texts = defaultdict(list)
-    for text, label in zip(chunks, labels):
+def get_central_chunks(embeddings, labels, chunk_texts, n_central=5):
+    print("Ищу центроиды кластеров")
+    cluster_embeddings = defaultdict(list)
+    cluster_indices = defaultdict(list)
+
+    for i, (emb, label) in enumerate(zip(embeddings, labels)):
         if label != -1:
-            cluster_texts[label].append(text)
+            cluster_embeddings[label].append(emb)
+            cluster_indices[label].append(i)
 
-    # for cluster_id, chunk_list in cluster_texts.items():
-    #     print(f"Кластер {cluster_id}:")
-    #     for chunk in chunk_list[:3]:
-    #         print(" -", chunk)
-    i = 0
-    for cluster_id, chunk_list in cluster_texts.items():
-        if i == 3:
-            break
-        i += 1
-        print(f"\n********************** {i} **********************")
-        prompt = f"""
-        На основе следующих текстов сформируй учебную тему.
-        Выдели:
-        1. Название темы (короткое)
-        2. Краткое описание темы
-        3. Подтемы или шаги изучения (список)
- 
-        Тексты:
-        {chunk_list[:5]} 
+    central_chunks = {}
+    for label, emb_list in cluster_embeddings.items():
+        emb_array = np.array(emb_list)
+        centroid = emb_array.mean(axis=0).reshape(1, -1)
+
+        distances = cdist(emb_array, centroid, metric='cosine').flatten()
+        closest_idx = np.argsort(distances)[:n_central]
+
+        texts = [chunk_texts[cluster_indices[label][i]] for i in closest_idx]
+        central_chunks[label] = texts
+
+    return central_chunks
+
+# TODO уточнить возвращаемую структуру списка
+def sort(topics, responses):
+    print("СортИрует")
+    prompt = f""" 
+        Упорядочи темы учебной программы логически: от базовых к продвинутым.
+        Темы:
+        {topics}
+        Верни отсортированный список.
         """
-
-        response = client.chat.completions.create(
-            model=models["Polaris"],
-            # max_tokens=1000,
-            temperature=0.2,
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
-        )
-        print(response.choices[0].message.content)
+    topics = ask_LLM(prompt)
+    return sorted(responses, key=lambda x: topics.index(x['topic']))
 
 
-process("src/BD1.pdf")
+
+
+def ask_LLM(prompt):
+    return client.chat.completions.create(
+        model=Config.LLM_MODELS["sherlock"],
+        temperature=0.2,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    ).choices[0].message.content
+
+
+def answer(embeddings, chunks, labels):
+    central_chunks = get_central_chunks(embeddings, labels, chunks, n_central=5)
+    topics = []
+    responses = []
+
+    cluster_keys = list(central_chunks.keys())
+    i = 0
+    while i < len(cluster_keys):
+        # if i == 3: break
+        cluster_id = cluster_keys[i]
+        chunk_list = central_chunks[cluster_id]
+        print(f"Итерация №{i}")
+        prompt = f"""
+               На основе следующих текстов сформируй учебную тему и сделай это чистым json объектом.
+               Выдели:
+               1. Название темы (короткое) topic
+               2. Краткое описание темы description
+               3. Подтемы или шаги изучения (список) subtopic
+
+               Тексты:
+               {". ".join(chunk_list)}
+               """
+        answer = ask_LLM(prompt)
+        try:
+            resp = json.loads(answer)
+            responses.append(resp)
+            topics.append(resp["topic"])
+            i += 1
+        except ValueError:
+            print(answer)
+            print(f"Ошибка JSON, повторим для кластера {cluster_id}...")
+
+    info = sort(topics, responses)
+    with open('example-finale.json', "a", encoding='utf-8') as f:
+        f.write("[")
+        f.write(json.dumps(info, ensure_ascii=False, indent=2))
+        f.write("]")
+
+
+process_cash(["src/BD.pdf", "src/MySQL_по_максимуму_2023.pdf"])
+# process(["src/BD.pdf", "src/MySQL_по_максимуму_2023.pdf"])
+# process("src/BD.pdf") # Для первого запуска
